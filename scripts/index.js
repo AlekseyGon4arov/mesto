5cda79374baca71fce36fb5856a54e4de91a0032
Alexey Goncharov <g-aleksey88@yandex.ru>
2022-11-02 13:17:17 +0300

    fix: исправить ошибки ПР4

--- scripts/index.js
+++ scripts/index.js
@@ -2,46 +2,39 @@
 let profElem = document.querySelector('.profile__button');
 let popupElem = document.querySelector('.popup');
 let popupCloseElem = popupElem.querySelector('.popup__close');
-let popupEdit = document.querySelector('.popup__edit');
+let name = document.querySelector('.profile__title');
+let job = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('input[name="name"]');
+let jobInput = document.querySelector('input[name="job"]');
+let formElement = document.querySelector('.popup__edit');
 
-// Функции для открытия и закрытия попапа
+// Переменные для изменения значаений
+let nameValue = name;
+let jobValue = job;
+
+// Функция открытия попап и переноса значений в форму
 function openPopup () {
+  nameInput.value = nameValue.textContent;
+  jobInput.value = jobValue.textContent;
   popupElem.classList.add('popup__open');
 };
 
+// Функция закрытия попап
 function closePopup () {
   popupElem.classList.remove('popup__open');
-  renderValues();
 };
 
+// Функция для переноса значений при нажатиии на кнопку Сохранить
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    nameValue.textContent = nameInput.value;
+    jobValue.textContent = jobInput.value;
+    closePopup ();
+}
+
+// События
 profElem.addEventListener('click', openPopup);
 
 popupCloseElem.addEventListener('click', closePopup);
 
-// Переменные для значения в профиле
-let nameValue = 'Жак-Ив Кусто';
-let jobValue = 'Исследователь океана';
-
-// Функция отрисовки профиля
-function renderValues () {
-  let name = document.querySelector('.profile__title');
-  let job = document.querySelector('.profile__subtitle');
-  let nameInput = document.querySelector('input[name="name"]');
-  let jobInput = document.querySelector('input[name="job"]');
-
-  name.textContent = nameValue;
-  nameInput.value = nameValue;
-
-  job.textContent = jobValue;
-  jobInput.value = jobValue;
-};
-
-renderValues();
-
-// Обработчик отправки формы
-popupEdit && popupEdit.addEventListener('submit', function(event){
-  event.preventDefault();
-  nameValue = event.target.querySelector('[name="name"]').value;
-  jobValue = event.target.querySelector('[name="job"]').value;
-  closePopup();
-});
+formElement.addEventListener('submit', formSubmitHandler);
