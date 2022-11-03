d0afe2e57f45360212445eec63c660c2b16c5655
Alexey Goncharov <g-aleksey88@yandex.ru>
2022-11-03 20:24:14 +0300

    fix: исправить ошибки ПР4

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,36 @@
 // Находим элементы в DOM
-let profElem = document.querySelector('.profile__button');
-let popupElem = document.querySelector('.popup');
-let popupCloseElem = popupElem.querySelector('.popup__close');
-let name = document.querySelector('.profile__title');
-let job = document.querySelector('.profile__subtitle');
-let nameInput = document.querySelector('input[name="name"]');
-let jobInput = document.querySelector('input[name="job"]');
-let formElement = document.querySelector('.popup__edit');
-
-// Переменные для изменения значаений
-let nameValue = name;
-let jobValue = job;
+let profileButton = document.querySelector('.profile__edit-button');
+let popupElement = document.querySelector('.popup');
+let popupCloseButton = popupElement.querySelector('.popup__close');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.popup__item_data_name');
+let jobInput = document.querySelector('.popup__item_data_job');
+let formElement = document.querySelector('.popup__form');
 
 // Функция открытия попап и переноса значений в форму
 function openPopup () {
-  nameInput.value = nameValue.textContent;
-  jobInput.value = jobValue.textContent;
-  popupElem.classList.add('popup__open');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popupElement.classList.add('popup_opened');
 };
 
 // Функция закрытия попап
 function closePopup () {
-  popupElem.classList.remove('popup__open');
+  popupElement.classList.remove('popup_opened');
 };
 
 // Функция для переноса значений при нажатиии на кнопку Сохранить
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameValue.textContent = nameInput.value;
-    jobValue.textContent = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     closePopup ();
 }
 
 // События
-profElem.addEventListener('click', openPopup);
+profileButton.addEventListener('click', openPopup);
 
-popupCloseElem.addEventListener('click', closePopup);
+popupCloseButton.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
